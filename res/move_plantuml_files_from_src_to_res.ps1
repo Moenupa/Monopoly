9a979e485c61b88ab2bb8488e1a3f0c4aa796030
Moenupa WANG <moenupa@outlook.com>
2021-10-21 23:13:11 +0800

    minor doc update

--- res/move_plantuml_files_from_src_to_res.ps1
+++ res/move_plantuml_files_from_src_to_res.ps1
@@ -1,6 +1,18 @@
+# this script
+#   1. centralizes the plantuml files (seperated in different folders) to ./res/plantuml/
+#   2. generates svgs and pngs to ./res/plantuml/png or ./res/plantuml/svg folder
+# ! Before running the script, make sure you have run "Sketch It" IDEA plugin first
+
+
+# assuming that the plantuml exists
+# move all *.plantuml files to ./res/plantuml folder
 cd ..
 Get-Location
 Get-ChildItem -Recurse -Include *.plantuml | Move-Item -Force -Destination ./res/plantuml
+
+# based on the plantuml, generate svgs and pngs
+# note that you must have downloaded the plantuml.jar package first
+# be aware of the directory where the plantuml.jar package is
 cd ./res/plantuml
 Get-Location
 java -jar C:/Github/plantuml.jar *.plantuml -o ./svg -tsvg
