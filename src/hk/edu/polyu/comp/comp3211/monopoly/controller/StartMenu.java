3bd88a58df8bedff08ccfd9a02c0dd808e2bf74d
ApotoxinElish <19079181d@connect.polyu.hk>
2021-11-13 02:23:20 +0800

    Update

--- src/hk/edu/polyu/comp/comp3211/monopoly/controller/StartMenu.java
+++ src/hk/edu/polyu/comp/comp3211/monopoly/controller/StartMenu.java
@@ -6,9 +6,13 @@ import hk.edu.polyu.comp.comp3211.monopoly.model.Board;
 import java.util.Scanner;
 
 public class StartMenu implements IBase {
-    /** Error message when detecting invalid option numbers */
+    /**
+     * Error message when detecting invalid option numbers
+     */
     private static final String ERR_INVALID_NUM_OF_Menu = "the option should only be 1-3 number";
-    /** Scanner for user input */
+    /**
+     * Scanner for user input
+     */
     private static Scanner in;
 
     /**
@@ -30,10 +34,12 @@ public class StartMenu implements IBase {
             try {
                 chooseOption(option);
                 break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("The option number should be 1-3");
             } catch (Exception e) {
                 // input 'num' is invalid
-                //                System.out.println("");
-                //                e.printStackTrace();
+                // System.out.println("");
+                e.printStackTrace();
             }
         }
     }
@@ -63,12 +69,16 @@ public class StartMenu implements IBase {
         }
     }
 
-    /** new game option */
+    /**
+     * new game option
+     */
     private static void startNewGame() {
         Main.setUI(new Game());
     }
 
-    /** load one of saved game */
+    /**
+     * load one of saved game
+     */
     private static void loadPreviousGame() throws Exception {
         String name = chooseStoredGame();
 
@@ -88,7 +98,8 @@ public class StartMenu implements IBase {
         if (savedGameName.length < 1) {
             System.out.println("No game has been stored");
             return null;
-        } else if (savedGameName.length == 1) return savedGameName[0];
+        } else if (savedGameName.length == 1)
+            return savedGameName[0];
 
         System.out.println("\nPrevious Game Selection");
         for (int i = 0; i < savedGameName.length; i++) {
@@ -105,7 +116,9 @@ public class StartMenu implements IBase {
         }
     }
 
-    /** quit game */
+    /**
+     * quit game
+     */
     private static void quitGame() {
         System.exit(0);
     }
