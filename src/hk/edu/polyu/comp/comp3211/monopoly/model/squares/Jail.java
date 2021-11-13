0d3dd846c140ab2654cf1653b8bdb0fcc512abf0
Moenupa WANG <moenupa@outlook.com>
2021-11-13 16:28:43 +0800

    Minor Update, Prepare for PR

--- src/hk/edu/polyu/comp/comp3211/monopoly/model/squares/Jail.java
+++ src/hk/edu/polyu/comp/comp3211/monopoly/model/squares/Jail.java
@@ -1,13 +1,14 @@
 package hk.edu.polyu.comp.comp3211.monopoly.model.squares;
 
 import hk.edu.polyu.comp.comp3211.monopoly.model.Player;
-
-import java.util.Random;
-import java.util.Scanner;
+import hk.edu.polyu.comp.comp3211.monopoly.Main;
 
 /** The In-Jail/Just-Visiting square of the board */
 public class Jail implements ISquare {
-    private static final int fine = -150;
+    private static final int FINE = -150;
+    private static final String CONFIRM_PATTERN = "^[nNyY]$";
+    private static final String CONFIRM_YES_PATTERN = "^[yY]$";
+    private static final String GET_CONFIRM_MSG = "Pay or not (y/n): ";
 
     private boolean test;
     private int[] diceResult = new int[2];
@@ -45,35 +46,34 @@ public class Jail implements ISquare {
         if (curInJail == 0) return;
 
         if (test) {
-            dices[0] = diceResult[0];
-            dices[1] = diceResult[1];
+            dices = diceResult;
         } else {
-            dices = new Random().ints(2, 1, 7).toArray();
+            dices[0] = player.rollDice();
+            dices[1] = player.rollDice();
         }
 
+        int stepsToMove = dices[0] + dices[1];
+
         if (dices[0] == dices[1]) { // if throw doubles
-            player.setInJail(0);
-            player.move(dices[0] + dices[1]);
-        } else { // if not throw doubles
-            if (curInJail == 3) { // if 3rd turn in jail
-                player.setInJail(0);
-                player.addMoney(fine);
-                player.move(dices[0] + dices[1]);
+            GetOutOfJail(player, stepsToMove);
+        } else {
+            // if not throw doubles
+            if (curInJail == 1) { // if 3rd turn in jail (the last)
+                PayFineAndGetOut(player, stepsToMove);
             } else { // if not 3rd turn in jail
                 // get whether pay
                 if (test) pay = payResult;
-                else {
-                    Scanner in = new Scanner(System.in);
-                    String inputString = in.nextLine();
-                    if (inputString.equals("Y")) pay = true;
-                    else pay = false;
-                }
+                String inputConfirm;
+                do {
+                    System.out.println(GET_CONFIRM_MSG);
+                    inputConfirm = Main.GetScanner().next();
+                } while (inputConfirm.matches(CONFIRM_PATTERN));
+
+                pay = inputConfirm.matches(CONFIRM_YES_PATTERN);
 
                 if (pay) {
-                    player.setInJail(0);
-                    player.addMoney(fine);
-                    player.move(dices[0] + dices[1]);
-                } else player.setInJail(curInJail + 1);
+                    PayFineAndGetOut(player, stepsToMove);
+                } else player.setInJail(curInJail - 1);
             }
         }
     }
@@ -83,5 +83,15 @@ public class Jail implements ISquare {
         this.diceResult[0] = dice0;
         this.diceResult[1] = dice1;
         this.payResult = pay;
+    }
+
+    private static void PayFineAndGetOut(Player player, int stepsToMove) {
+        // pay a fine and then get out of jail
+        player.addMoney(FINE);
+        GetOutOfJail(player, stepsToMove);
+    }
+    private static void GetOutOfJail(Player player, int stepsToMove) {
+        player.setInJail(0);
+        player.move(stepsToMove);
     }
 }
